73c5ae5c5e9a7cd5340414daf849a7ab54345246
magmi362 <magnus.mickelsson@ucr.uu.se>
2017-09-10 20:09:45 +0200

    Web controller

--- src/main/java/pokeraidbot/BotController.java
+++ src/main/java/pokeraidbot/BotController.java
@@ -1,8 +1,11 @@
 package pokeraidbot;
 
-//@RestController
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BotController {
-    //    @RequestMapping("/")
+    @RequestMapping("/")
     public String index() {
         return "Pokeraidbot says hi! To add me to your server, please use this link: https://discordapp.com/oauth2/authorize?&client_id=354980212112621570&scope=bot&permissions=0 ..." +
                 "\nUsage in chat: !raid usage";
