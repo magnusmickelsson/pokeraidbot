7ddf9a881d7bdc547aeb95ddce017a3cc23a0433
magmi362 <magnus.mickelsson@ucr.uu.se>
2017-09-10 22:14:13 +0200

    Less logging, cleanup

--- src/main/java/pokeraidbot/infrastructure/CounterTextFileParser.java
+++ src/main/java/pokeraidbot/infrastructure/CounterTextFileParser.java
@@ -15,7 +15,6 @@ public class CounterTextFileParser {
 
     public CounterTextFileParser(String path, String pokemonName, PokemonRepository pokemonRepository) {
         try {
-//            System.out.println("Parsing counters for " + pokemonName + " ...");
             final InputStream inputStream = CounterTextFileParser.class.getResourceAsStream(path + "/" + pokemonName.toLowerCase() + ".txt");
             final InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
             BufferedReader br = new BufferedReader(inputStreamReader);
@@ -74,7 +73,6 @@ public class CounterTextFileParser {
         } catch (Throwable e) {
             throw new RuntimeException(e.getMessage());
         }
-        System.out.println("Parsed counters OK.");
     }
 
     public Set<CounterPokemon> getBestCounters() {
