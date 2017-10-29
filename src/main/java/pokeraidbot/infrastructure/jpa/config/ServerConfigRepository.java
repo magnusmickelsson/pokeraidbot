b1798439a28e5f2cd387c652f44d376fb73a15fb
magmi362 <magnus.mickelsson@ucr.uu.se>
2017-10-29 21:15:44 +0100

    Requires new transactions for this repo.

--- src/main/java/pokeraidbot/infrastructure/jpa/config/ServerConfigRepository.java
+++ src/main/java/pokeraidbot/infrastructure/jpa/config/ServerConfigRepository.java
@@ -1,12 +1,13 @@
 package pokeraidbot.infrastructure.jpa.config;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Transactional
+@Transactional(propagation = Propagation.REQUIRES_NEW)
 public interface ServerConfigRepository extends JpaRepository<Config, String> {
     Config findByServer(String server);
 
