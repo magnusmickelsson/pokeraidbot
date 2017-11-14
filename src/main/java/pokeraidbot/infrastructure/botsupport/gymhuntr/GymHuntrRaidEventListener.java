1683979a4d833090cd49a77b37754f37faf72088
magmi362 <magnus.mickelsson@ucr.uu.se>
2017-11-14 23:34:22 +0100

    Trying to adapt to having both gymhuntr and pokealarm as possible raid providers, also egg detection for tier 5.

--- src/main/java/pokeraidbot/infrastructure/botsupport/gymhuntr/GymHuntrRaidEventListener.java
+++ src/main/java/pokeraidbot/infrastructure/botsupport/gymhuntr/GymHuntrRaidEventListener.java
@@ -3,6 +3,7 @@ package pokeraidbot.infrastructure.botsupport.gymhuntr;
 import main.BotServerMain;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.entities.MessageEmbed;
+import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.Event;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.EventListener;
@@ -26,6 +27,8 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.*;
 import java.util.concurrent.ExecutorService;
+
+import static pokeraidbot.Utils.printTime;
 
 public class GymHuntrRaidEventListener implements EventListener {
     private static final Logger LOGGER = LoggerFactory.getLogger(GymHuntrRaidEventListener.class);
@@ -55,51 +58,36 @@ public class GymHuntrRaidEventListener implements EventListener {
     public void onEvent(Event event) {
         if (event instanceof GuildMessageReceivedEvent) {
             GuildMessageReceivedEvent guildEvent = (GuildMessageReceivedEvent) event;
-            if (guildEvent.getAuthor().isBot() && StringUtils.containsIgnoreCase(
-                    guildEvent.getAuthor().getName(), "gymhuntrbot")) {
+            final User messageAuthor = guildEvent.getAuthor();
+            if (isUserGymhuntrBot(messageAuthor) || isUserPokeAlarmBot(messageAuthor)) {
                 final String serverName = guildEvent.getGuild().getName().toLowerCase();
-                Config config = serverConfigRepository.getConfigForServer(serverName);
                 final List<MessageEmbed> embeds = guildEvent.getMessage().getEmbeds();
                 if (embeds != null && embeds.size() > 0) {
                     for (MessageEmbed embed : embeds) {
                         final LocalDateTime currentDateTime = clockService.getCurrentDateTime();
                         final String description = embed.getDescription();
                         final String title = embed.getTitle();
-                        // todo: detect "starting soon" for tier 5 bosses, and have mapping of what legendary boss
-                        // is active for the current zone
-                        if (StringUtils.containsIgnoreCase(title, "has started!")) {
-                            List<String> newRaidArguments = argumentsToCreateRaid(description, clockService);
-//                            guildEvent.getMessage().getChannel().sendMessage().queue();
+                        List<String> newRaidArguments;
+                        if (isUserGymhuntrBot(messageAuthor)) {
+                            newRaidArguments = gymhuntrArgumentsToCreateRaid(title, description, clockService);
+                        } else if (isUserPokeAlarmBot(messageAuthor)) {
+                            newRaidArguments = pokeAlarmArgumentsToCreateRaid(title, description, clockService);
+                        } else {
+                            newRaidArguments = new ArrayList<>();
+                        }
+                        if (newRaidArguments.size() > 0) {
                             // todo: arguments checking
                             final Iterator<String> iterator = newRaidArguments.iterator();
                             final String gym = iterator.next();
                             final String pokemon = iterator.next();
                             final String time = iterator.next();
                             final Pokemon raidBoss = pokemonRepository.getByName(pokemon);
-                            config = serverConfigRepository.getConfigForServer(serverName);
+                            final Config config = serverConfigRepository.getConfigForServer(serverName);
                             final Gym raidGym = gymRepository.findByName(gym, config.getRegion());
                             final LocalDate currentDate = currentDateTime.toLocalDate();
                             final LocalDateTime endOfRaid = LocalDateTime.of(currentDate,
                                     LocalTime.parse(time, Utils.timeParseFormatter));
-                            final boolean moreThan10MinutesLeftOnRaid = endOfRaid.isAfter(currentDateTime.plusMinutes(10));
-                            if (moreThan10MinutesLeftOnRaid) {
-                                // todo: check if raid already exists
-                                final Raid raidToCreate = new Raid(raidBoss,
-                                        endOfRaid,
-                                        raidGym,
-                                        localeService, config.getRegion());
-                                final Raid createdRaid = raidRepository.newRaid(guildEvent.getAuthor(), raidToCreate);
-                                final Locale locale = config.getLocale();
-                                final MessageEmbed messageEmbed = new EmbedBuilder().setTitle(null, null)
-                                        .setDescription(localeService.getMessageFor(LocaleService.NEW_RAID_CREATED,
-                                                locale, createdRaid.toString(locale))).build();
-                                guildEvent.getMessage().getChannel().sendMessage(messageEmbed).queue(m -> {
-                                    LOGGER.info("Raid created via Gymhuntr integration: " + createdRaid);
-                                });
-                            } else {
-                                LOGGER.debug("Skipped creating raid at " + gym +
-                                        ", less than 10 minutes remaining on it.");
-                            }
+                            handleRaidFromIntegration(guildEvent, raidBoss, raidGym, endOfRaid, config, clockService);
                         }
                     }
                 }
@@ -107,16 +95,98 @@ public class GymHuntrRaidEventListener implements EventListener {
         }
     }
 
-    public static List<String> argumentsToCreateRaid(String description, ClockService clockService) {
-        final String[] firstPass = description.replaceAll("[*]", "").replaceAll("[.]", "")
-                .replaceAll("Raid Ending: ", "").split("\n");
-        final String[] timeArguments = firstPass[3].replaceAll("hours ", "")
-                .replaceAll("min ", "").replaceAll("sec", "").split(" ");
-        final String timeString = Utils.printTime(clockService.getCurrentTime()
-                .plusHours(Long.parseLong(timeArguments[0]))
-                .plusMinutes(Long.parseLong(timeArguments[1]))
-                .plusSeconds(Long.parseLong(timeArguments[2])));
-        final String[] secondPass = new String[]{firstPass[0], firstPass[1], timeString};
-        return Arrays.asList(secondPass);
+    public void handleRaidFromIntegration(GuildMessageReceivedEvent guildEvent, Pokemon raidBoss, Gym raidGym,
+                                          LocalDateTime endOfRaid, Config config, ClockService clockService) {
+        User messageAuthor = guildEvent.getAuthor();
+        LocalDateTime currentDateTime = clockService.getCurrentDateTime();
+        final boolean moreThan10MinutesLeftOnRaid = endOfRaid.isAfter(currentDateTime.plusMinutes(10));
+        if (moreThan10MinutesLeftOnRaid) {
+            final Raid raidToCreate = new Raid(raidBoss,
+                    endOfRaid,
+                    raidGym,
+                    localeService, config.getRegion());
+            final Raid createdRaid;
+            try {
+                createdRaid = raidRepository.newRaid(messageAuthor, raidToCreate);
+                final Locale locale = config.getLocale();
+                final MessageEmbed messageEmbed = new EmbedBuilder().setTitle(null, null)
+                        .setDescription(localeService.getMessageFor(LocaleService.NEW_RAID_CREATED,
+                                locale, createdRaid.toString(locale))).build();
+                // todo: fetch from config what channel to post this message in
+                guildEvent.getMessage().getChannel().sendMessage(messageEmbed).queue(m -> {
+                    LOGGER.info("Raid created via Bot integration: " + createdRaid);
+                });
+            } catch (Throwable t) {
+                LOGGER.warn("Exception when trying to create raid via botintegration: " +
+                        t.getMessage());
+            }
+        } else {
+            LOGGER.debug("Skipped creating raid at " + raidGym +
+                    ", less than 10 minutes remaining on it.");
+        }
+    }
+
+    public static boolean isUserPokeAlarmBot(User user) {
+        return user.isBot() && user.getName().equalsIgnoreCase("umu_raids");
+    }
+
+    public static boolean isUserGymhuntrBot(User user) {
+        return user.isBot() && StringUtils.containsIgnoreCase(
+                user.getName(), "gymhuntrbot");
+    }
+
+    public static List<String> pokeAlarmArgumentsToCreateRaid(String title, String description,
+                                                              ClockService clockService) {
+        String gym, pokemon, timeString;
+        if (title.contains("Raid is available")) {
+            final String[] titleSplit = title.replaceAll("!", "").split(" ");
+            pokemon = titleSplit[titleSplit.length - 1];
+            final String[] descriptionSplit = description.split(" ");
+            timeString = printTime(LocalTime.parse(descriptionSplit[descriptionSplit.length - 3]));
+            final String[] descSplit = description.split("has a raid and is available until");
+            gym = descSplit[0].trim();
+        } else if (title.contains("Raid is incoming!") && description.contains("level 5 raid will hatch")){
+            pokemon = "Raikou";
+            final String[] descriptionSplit = description.split(" ");
+            timeString = printTime(LocalTime.parse(descriptionSplit[descriptionSplit.length - 3])
+                    .plusMinutes(Utils.RAID_DURATION_IN_MINUTES));
+            gym = description.split("has a level 5 raid")[0].trim();
+        } else {
+            return new ArrayList<>(); // We shouldn't create a raid for this case
+        }
+        return Arrays.asList(new String[]{gym, pokemon, timeString});
+    }
+
+    public static List<String> gymhuntrArgumentsToCreateRaid(String title, String description,
+                                                             ClockService clockService) {
+        String gym, pokemon, timeString;
+        if (title.contains("Raid has started!")) {
+            final String[] firstPass = description.replaceAll("[*]", "").replaceAll("[.]", "")
+                    .replaceAll("Raid Ending: ", "").split("\n");
+            final String[] timeArguments = firstPass[3].replaceAll("hours ", "")
+                    .replaceAll("min ", "").replaceAll("sec", "").split(" ");
+            timeString = printTime(clockService.getCurrentTime()
+                    .plusHours(Long.parseLong(timeArguments[0]))
+                    .plusMinutes(Long.parseLong(timeArguments[1]))
+                    .plusSeconds(Long.parseLong(timeArguments[2])));
+            gym = firstPass[0].trim();
+            pokemon = firstPass[1].trim();
+        } else if (title.contains("Level 5 Raid is starting soon!")){
+            final String[] firstPass = description.replaceAll("[*]", "").replaceAll("[.]", "")
+                    .replaceAll("Raid Starting: ", "").split("\n");
+            pokemon = "Raikou"; // todo: fetch from some repo keeping track of what tier 5 boss is active for the region?
+            gym = firstPass[0].trim();
+            final String[] timeArguments = firstPass[1].replaceAll("hours ", "")
+                    .replaceAll("min ", "").replaceAll("sec", "").split(" ");
+            timeString = printTime(clockService.getCurrentTime()
+                    .plusHours(Long.parseLong(timeArguments[0]))
+                    .plusMinutes(Long.parseLong(timeArguments[1]))
+                    .plusSeconds(Long.parseLong(timeArguments[2]))
+                    .plusMinutes(Utils.RAID_DURATION_IN_MINUTES));
+        } else {
+            return new ArrayList<>(); // = We shouldn't create this raid, since it is a non-tier 5 egg
+        }
+        final String[] argumentsInOrder = new String[]{gym, pokemon, timeString};
+        return Arrays.asList(argumentsInOrder);
     }
 }
--- src/test/java/pokeraidbot/infrastructure/botsupport/gymhuntr/GymHuntrRaidEventListenerTest.java
+++ src/test/java/pokeraidbot/infrastructure/botsupport/gymhuntr/GymHuntrRaidEventListenerTest.java
@@ -1,9 +1,11 @@
 package pokeraidbot.infrastructure.botsupport.gymhuntr;
 
+import org.junit.Before;
 import org.junit.Test;
 import pokeraidbot.Utils;
 import pokeraidbot.domain.config.ClockService;
 
+import java.time.LocalTime;
 import java.util.Iterator;
 import java.util.List;
 
@@ -11,19 +13,68 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class GymHuntrRaidEventListenerTest {
+
+    private ClockService clockService;
+
+    @Before
+    public void setUp() throws Exception {
+        clockService = new ClockService();
+    }
+
     @Test
-    public void testParseDescriptionIntoArguments() throws Exception {
+    public void testParseGymhuntrMessageForBossIntoArguments() throws Exception {
         final String typeOfMessage = "**Sunnerstakyrkan.**\n" +
                 "Ninetales\n" +
                 "CP: 14914.\n" +
                 "*Raid Ending: 0 hours 15 min 59 sec*";
-        final ClockService clockService = new ClockService();
-        final List<String> arguments = GymHuntrRaidEventListener.argumentsToCreateRaid(typeOfMessage, clockService);
+        final List<String> arguments = GymHuntrRaidEventListener.gymhuntrArgumentsToCreateRaid(
+                "Level 3 Raid has started!", typeOfMessage, clockService);
         assertThat(arguments.size(), is(3));
         final Iterator<String> iterator = arguments.iterator();
         assertThat(iterator.next(), is("Sunnerstakyrkan"));
         assertThat(iterator.next(), is("Ninetales"));
         assertThat(iterator.next(), is(Utils.printTime(clockService.getCurrentTime()
                 .plusMinutes(15).plusSeconds(59))));
+    }
+
+    @Test
+    public void testParseGymhuntrMessageForTier5EggIntoArguments() throws Exception {
+        final String typeOfMessage = "**Staty utanför Svedbergs Laboratoriet .**\n" +
+                "*Raid Starting: 0 hours 15 min 59 sec*";
+        final List<String> arguments = GymHuntrRaidEventListener.gymhuntrArgumentsToCreateRaid(
+                "Level 5 Raid is starting soon!", typeOfMessage, clockService);
+        assertThat(arguments.size(), is(3));
+        final Iterator<String> iterator = arguments.iterator();
+        assertThat(iterator.next(), is("Staty utanför Svedbergs Laboratoriet"));
+        assertThat(iterator.next(), is("Raikou"));
+        assertThat(iterator.next(), is(Utils.printTime(clockService.getCurrentTime()
+                .plusMinutes(15).plusSeconds(59).plusMinutes(Utils.RAID_DURATION_IN_MINUTES))));
+    }
+
+    @Test
+    public void testParsePokeAlarmBossMessageIntoArguments() throws Exception {
+        clockService.setMockTime(LocalTime.of(19, 0, 0));
+        final List<String> arguments = GymHuntrRaidEventListener.pokeAlarmArgumentsToCreateRaid(
+                "Level 5 Raid is available against Raikou!",
+                "T3 Center has a raid and is available until 19:16:10 (46m 2s).", clockService);
+        assertThat(arguments.size(), is(3));
+        final Iterator<String> iterator = arguments.iterator();
+        assertThat(iterator.next(), is("T3 Center"));
+        assertThat(iterator.next(), is("Raikou"));
+        assertThat(iterator.next(), is(Utils.printTime(clockService.getCurrentTime()
+                .plusMinutes(16).plusSeconds(10))));
+    }
+
+    @Test
+    public void testParsePokeAlarmTier5EggMessageIntoArguments() throws Exception {
+        clockService.setMockTime(LocalTime.of(18, 0, 0));
+        final List<String> arguments = GymHuntrRaidEventListener.pokeAlarmArgumentsToCreateRaid(
+                "Raid is incoming!", "T3 Center has a level 5 raid will hatch 18:31:10 (54m 32s).", clockService);
+        assertThat(arguments.size(), is(3));
+        final Iterator<String> iterator = arguments.iterator();
+        assertThat(iterator.next(), is("T3 Center"));
+        assertThat(iterator.next(), is("Raikou"));
+        assertThat(iterator.next(), is(Utils.printTime(clockService.getCurrentTime()
+                .plusMinutes(31).plusSeconds(10).plusMinutes(Utils.RAID_DURATION_IN_MINUTES))));
     }
 }
