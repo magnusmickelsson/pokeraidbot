e5c68973420aa5c12990d82811426b79ce747a08
magmi362 <magnus.mickelsson@ucr.uu.se>
2018-05-08 22:55:29 +0200

    Gym data update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 =====
 * Snooze button for raid groups
 
+1.8.33 (2018-05-08)
+=====
+* Gym data update
+
 1.8.32 (2018-05-07)
 =====
 * Gym data for Vindeln
--- src/test/java/pokeraidbot/domain/GymRepositoryTest.java
+++ src/test/java/pokeraidbot/domain/GymRepositoryTest.java
@@ -112,7 +112,7 @@ public class GymRepositoryTest {
 
     @Test
     public void allGymsAreReadForUppsala() {
-        assertThat(repo.getAllGymsForRegion("uppsala").size(), is(223));
+        assertThat(repo.getAllGymsForRegion("uppsala").size(), is(224));
     }
 
     @Test
