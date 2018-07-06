fd5f3944f473fa803dea6ca5564d1962593ac6ce
magmi362 <magnus.mickelsson@ucr.uu.se>
2018-07-06 11:18:30 +0200

    Gym data update

--- src/test/java/pokeraidbot/domain/GymRepositoryTest.java
+++ src/test/java/pokeraidbot/domain/GymRepositoryTest.java
@@ -127,7 +127,7 @@ public class GymRepositoryTest {
 
     @Test
     public void allGymsAreReadForUmea() {
-        assertThat(repo.getAllGymsForRegion("umeå").size(), is(132));
+        assertThat(repo.getAllGymsForRegion("umeå").size(), is(133));
     }
 
     @Test
