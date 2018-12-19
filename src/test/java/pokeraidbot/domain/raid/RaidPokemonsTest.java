8bbf7b9e87e2425e36a5a1169692b7b48607b917
magmi362 <magnus.mickelsson@ucr.uu.se>
2018-12-19 07:23:00 +0100

    Fix test case

--- src/test/java/pokeraidbot/domain/raid/RaidPokemonsTest.java
+++ src/test/java/pokeraidbot/domain/raid/RaidPokemonsTest.java
@@ -145,7 +145,7 @@ public class RaidPokemonsTest {
         assertThat(raidInfo.getBossTier(), is(4));
         counters = strategyService.getCounters(raidInfo.getPokemon());
         assertThat(counters.getGoodCounters().size(), is(4));
-        assertThat(counters.getSupremeCounters().size(), is(1));
+        assertThat(counters.getSupremeCounters().size(), is(2));
 
         raidInfo = strategyService.getRaidInfo(pokemonRepository.search("celebi", null));
         assertNotNull(raidInfo);
