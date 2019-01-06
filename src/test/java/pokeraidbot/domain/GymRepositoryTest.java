9d723d72f8c0ec656864428bd46a8c796517a5ed
magmi362 <magnus.mickelsson@ucr.uu.se>
2019-01-06 22:01:29 +0100

    Bälinge gyms for Uppsala

--- src/test/java/pokeraidbot/domain/GymRepositoryTest.java
+++ src/test/java/pokeraidbot/domain/GymRepositoryTest.java
@@ -112,7 +112,7 @@ public class GymRepositoryTest {
 
     @Test
     public void allGymsAreReadForUppsala() {
-        assertThat(repo.getAllGymsForRegion("uppsala").size(), is(234));
+        assertThat(repo.getAllGymsForRegion("uppsala").size(), is(237));
     }
 
     @Test
