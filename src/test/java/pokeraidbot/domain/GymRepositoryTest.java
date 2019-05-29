3c6bf83ac6271c6b1a5a482f0fbf4b7246f6b471
magmi362 <magnus.mickelsson@ucr.uu.se>
2019-05-29 22:57:13 +0200

    Update test to match gym changes from Umeå

--- src/test/java/pokeraidbot/domain/GymRepositoryTest.java
+++ src/test/java/pokeraidbot/domain/GymRepositoryTest.java
@@ -127,7 +127,7 @@ public class GymRepositoryTest {
 
     @Test
     public void allGymsAreReadForUmea() {
-        assertThat(repo.getAllGymsForRegion("umeå").size(), is(181));
+        assertThat(repo.getAllGymsForRegion("umeå").size(), is(190));
     }
 
     @Test
