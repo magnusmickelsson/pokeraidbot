e76b06e61c852b05e529d90d879abc3e47098f89
magmi362 <magnus.mickelsson@ucr.uu.se>
2019-09-17 21:25:31 +0200

    raid duration 45 mins

--- src/main/java/pokeraidbot/Utils.java
+++ src/main/java/pokeraidbot/Utils.java
@@ -33,7 +33,7 @@ public class Utils {
     public static final DateTimeFormatter timePrintFormatter = DateTimeFormatter.ofPattern("HH:mm");
     public static final DateTimeFormatter dateAndTimePrintFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
     public static final int HIGH_LIMIT_FOR_SIGNUPS = 20;
-    public static final int RAID_DURATION_IN_MINUTES = 90;
+    public static final int RAID_DURATION_IN_MINUTES = 45;
     public static final int EX_RAID_DURATION_IN_MINUTES = 45;
     private static final String EX_RAID_BOSS = "deoxys";
     private static ClockService clockService = new ClockService();
