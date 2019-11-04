5f1863de82057c6c750181ebb99c4a92fff5c52d
magmi362 <magnus.mickelsson@ucr.uu.se>
2019-11-04 21:31:52 +0100

    New gyms

--- src/test/java/pokeraidbot/domain/GymRepositoryTest.java
+++ src/test/java/pokeraidbot/domain/GymRepositoryTest.java
@@ -112,7 +112,7 @@ public class GymRepositoryTest {
 
     @Test
     public void allGymsAreReadForUppsala() {
-        assertThat(repo.getAllGymsForRegion("uppsala").size(), is(253));
+        assertThat(repo.getAllGymsForRegion("uppsala").size(), is(255));
     }
 
     @Test
