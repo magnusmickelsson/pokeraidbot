a48149f800a125bfcbd4befd886d2afb159c1a90
jmillert <johan.millert@gmail.com>
2020-02-20 10:15:36 +0100

    Gym data

--- src/test/java/pokeraidbot/domain/GymRepositoryTest.java
+++ src/test/java/pokeraidbot/domain/GymRepositoryTest.java
@@ -112,7 +112,7 @@ public class GymRepositoryTest {
 
     @Test
     public void allGymsAreReadForUppsala() {
-        assertThat(repo.getAllGymsForRegion("uppsala").size(), is(290));
+        assertThat(repo.getAllGymsForRegion("uppsala").size(), is(292));
     }
 
     @Test
