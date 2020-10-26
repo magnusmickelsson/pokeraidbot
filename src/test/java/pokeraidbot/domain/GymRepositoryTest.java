cd18ee9d2fd98986fa547fe5c402182a264b090c
Magnus Mickelsson <magnus.mickelsson@gmail.com>
2020-10-26 16:10:35 +0100

    New gyms

--- src/test/java/pokeraidbot/domain/GymRepositoryTest.java
+++ src/test/java/pokeraidbot/domain/GymRepositoryTest.java
@@ -123,7 +123,7 @@ public class GymRepositoryTest {
 
     @Test
     public void allGymsAreReadForUppsala() {
-        assertThat(repo.getAllGymsForRegion("uppsala").size(), is(348));
+        assertThat(repo.getAllGymsForRegion("uppsala").size(), is(350));
     }
 
     @Test
