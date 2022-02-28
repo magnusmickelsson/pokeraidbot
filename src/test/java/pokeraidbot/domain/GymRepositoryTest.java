3fbdc720906966fb4b99e8548e5b769cb41d0026
Johan Millert <johan.millert@ucr.uu.se>
2022-02-28 12:07:12 +0100

    Gym data

--- src/test/java/pokeraidbot/domain/GymRepositoryTest.java
+++ src/test/java/pokeraidbot/domain/GymRepositoryTest.java
@@ -123,7 +123,7 @@ public class GymRepositoryTest {
 
     @Test
     public void allGymsAreReadForUppsala() {
-        assertThat(repo.getAllGymsForRegion("uppsala").size(), is(465));
+        assertThat(repo.getAllGymsForRegion("uppsala").size(), is(469));
     }
 
     @Test
